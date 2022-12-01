c7dd9e4ecb84ba90291118dd70300ba53167a439
Ryan Silvera <ryan.silvera@icloud.com>
2022-12-01 12:41:02 -0500

    Form contents get added to array

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-let list = document.getElementById('books')
-let form = document.getElementById('form')
-let button = document.getElementById('button')
+const list = document.getElementById('books')
+const form = document.getElementById('form')
+const addBookButton = document.getElementById('button')
+const submitButton = document.getElementsByName('submit')
 
 let myLibrary = []
 
@@ -12,19 +13,22 @@ function book (title, author, pages) {
 }
 
 // function for adding things to the array
-const addBookToLibrary = () => {
-    let userTitle = prompt('What is the title?')
-    let userAuthor = prompt('Who is the author?')
-    let userPages = prompt('How many pages are there?')
+// const addBookToLibrary = () => {
+//     let userTitle = prompt('What is the title?')
+//     let userAuthor = prompt('Who is the author?')
+//     let userPages = prompt('How many pages are there?')
 
-    bookInfo = new book(userTitle, userAuthor, userPages)
-    return bookInfo
-}
+//     bookInfo = new book(userTitle, userAuthor, userPages)
+//     return bookInfo
+// }
 
 // myLibrary.push(addBookToLibrary())
 // myLibrary.push(addBookToLibrary())
 // console.table(myLibrary)
 
+let bookTitleForm = undefined
+let bookAuthorForm = undefined
+let bookPagesForm = undefined
 
 // function for looping through array and displaying it in html
 for (books of myLibrary){
@@ -45,20 +49,22 @@ for (books of myLibrary){
 const createForm = () => {
     let makeForm = document.createElement('div')
     
-    let bookTitleForm = document.createElement('input')
+    bookTitleForm = document.createElement('input')
     bookTitleForm.setAttribute('type', 'text')
     bookTitleForm.setAttribute('placeholder', 'Title')
-    let bookAuthorForm = document.createElement('input')
+    bookAuthorForm = document.createElement('input')
     bookAuthorForm.setAttribute('type', 'text')
     bookAuthorForm.setAttribute('placeholder', 'Author')
-    let bookPagesForm = document.createElement('input')
+    bookPagesForm = document.createElement('input')
     bookPagesForm.setAttribute('type', 'number')
     bookPagesForm.setAttribute('placeholder', 'Pages')
     let bookCheck = document.createElement('input')
     bookCheck.setAttribute('type', 'checkbox')
+    // bookCheck.setAttribute('name', 'Have you read this book?')
     let formSubmit = document.createElement('input')
     formSubmit.setAttribute('type', 'submit')
-
+    formSubmit.setAttribute('name', 'submit')
+    
     form.innerHTML = ''
     form.appendChild(makeForm)
     makeForm.appendChild(bookTitleForm)
@@ -66,11 +72,28 @@ const createForm = () => {
     makeForm.appendChild(bookPagesForm)
     makeForm.appendChild(bookCheck)
     makeForm.appendChild(formSubmit)
+    submitButton[0].addEventListener('click', storeInArray)
+    console.log(submitButton[0])
+
+    return bookTitleForm, bookAuthorForm, bookPagesForm
 }
 
-// EventListener
-// button.addEventListener('click', function(){
-//     console.log('This button is working')
-// })
+// Function for storing books in array
+const storeInArray = () => {
+    let userTitle = bookTitleForm.value
+    let userAuthor = bookAuthorForm.value
+    let userPages = bookPagesForm.value
 
-button.addEventListener('click', createForm)
+    bookInfo = new book(userTitle, userAuthor, userPages)
+    myLibrary.push(bookInfo)
+    console.table(myLibrary)
+}
+
+
+// Function for clearing form
+const clear = () => {
+    
+}
+
+addBookButton.addEventListener('click', createForm)
+// submitButton.addEventListener('click', storeInArray)
