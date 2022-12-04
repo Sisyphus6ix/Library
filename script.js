654841d3326821f1f8c83f1e22640acfa08f8798
Ryan Silvera <ryan.silvera@icloud.com>
2022-12-04 17:28:42 -0500

    Buttons and book object correspond

--- script.js
+++ script.js
@@ -1,97 +1,109 @@
-const list = document.getElementById('books')
-const form = document.getElementById('form')
-const addBookButton = document.getElementById('button')
-const submitButton = document.getElementsByName('submit')
+const list = document.getElementById("books");
+const form = document.getElementById("form");
+const addBookButton = document.getElementById("button");
+const submitButton = document.getElementsByName("submit");
 
-let myLibrary = []
+let myLibrary = [];
 
 //constructor for creating books
-function book (title, author, pages) {
-    this.title = title
-    this.author = author
-    this.pages = pages
+function book(title, author, pages) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
 }
 
 // Globally setting values
-let bookTitleForm = undefined
-let bookAuthorForm = undefined
-let bookPagesForm = undefined
-
-
+let bookTitleForm = undefined;
+let bookAuthorForm = undefined;
+let bookPagesForm = undefined;
 
 // Function for making form
 const createForm = () => {
-    // Creating div for form
-    let makeForm = document.createElement('div')
+  // Creating div for form
+  let makeForm = document.createElement("div");
 
-    // Form contents 
-    bookTitleForm = document.createElement('input')
-    bookTitleForm.setAttribute('type', 'text')
-    bookTitleForm.setAttribute('placeholder', 'Title')
-    bookAuthorForm = document.createElement('input')
-    bookAuthorForm.setAttribute('type', 'text')
-    bookAuthorForm.setAttribute('placeholder', 'Author')
-    bookPagesForm = document.createElement('input')
-    bookPagesForm.setAttribute('type', 'number')
-    bookPagesForm.setAttribute('placeholder', 'Pages')
-    let bookCheck = document.createElement('input')
-    bookCheck.setAttribute('type', 'checkbox')
-    let formSubmit = document.createElement('input')
-    formSubmit.setAttribute('type', 'submit')
-    formSubmit.setAttribute('name', 'submit')
-    
-    // Adding form to div
-    form.innerHTML = ''
-    form.appendChild(makeForm)
-    makeForm.appendChild(bookTitleForm)
-    makeForm.appendChild(bookAuthorForm)
-    makeForm.appendChild(bookPagesForm)
-    makeForm.appendChild(bookCheck)
-    makeForm.appendChild(formSubmit)
+  // Form contents
+  bookTitleForm = document.createElement("input");
+  bookTitleForm.setAttribute("type", "text");
+  bookTitleForm.setAttribute("placeholder", "Title");
+  bookAuthorForm = document.createElement("input");
+  bookAuthorForm.setAttribute("type", "text");
+  bookAuthorForm.setAttribute("placeholder", "Author");
+  bookPagesForm = document.createElement("input");
+  bookPagesForm.setAttribute("type", "number");
+  bookPagesForm.setAttribute("placeholder", "Pages");
+  let bookCheck = document.createElement("input");
+  bookCheck.setAttribute("type", "checkbox");
+  let formSubmit = document.createElement("input");
+  formSubmit.setAttribute("type", "submit");
+  formSubmit.setAttribute("name", "submit");
 
-    // EventListener for using submit button in form
-    submitButton[0].addEventListener('click', storeInArray)
+  // Adding form to div
+  form.innerHTML = "";
+  form.appendChild(makeForm);
+  makeForm.appendChild(bookTitleForm);
+  makeForm.appendChild(bookAuthorForm);
+  makeForm.appendChild(bookPagesForm);
+  makeForm.appendChild(bookCheck);
+  makeForm.appendChild(formSubmit);
 
-    // returning variables to update their value
-    return bookTitleForm, bookAuthorForm, bookPagesForm
-}
+  // EventListener for using submit button in form
+  submitButton[0].addEventListener("click", storeInArray);
+
+  // returning variables to update their value
+  return bookTitleForm, bookAuthorForm, bookPagesForm;
+};
 
 // Function for storing books in array
 const storeInArray = () => {
-    let userTitle = bookTitleForm.value
-    let userAuthor = bookAuthorForm.value
-    let userPages = bookPagesForm.value
+  let userTitle = bookTitleForm.value;
+  let userAuthor = bookAuthorForm.value;
+  let userPages = bookPagesForm.value;
 
-    bookInfo = new book(userTitle, userAuthor, userPages)
-    myLibrary.push(bookInfo)
-    console.table(myLibrary)
-    form.innerHTML = ''
+  bookInfo = new book(userTitle, userAuthor, userPages);
+  myLibrary.push(bookInfo);
+  console.table(myLibrary);
+  form.innerHTML = "";
 
-    updatingHTML()
-}
+  updatingHTML();
+};
 
 // Function for looping through array and displaying it in html
 const updatingHTML = () => {
-    let bookDiv = document.createElement('div')
-    let bookTitle = document.createElement('h5')
-    let bookAuthor = document.createElement('p')
-    let bookPages = document.createElement('p')
-    let removeBtn = document.createElement('button')
-    removeBtn.setAttribute('type', 'button')
-    removeBtn.setAttribute('name', 'removeBtn')
-    removeBtn.insertAdjacentText('beforeend', 'Remove')
+  list.innerHTML = "";
 
-    for (books of myLibrary.slice(-1)){
-        bookTitle.innerText = books.title
-        bookAuthor.innerText = books.author
-        bookPages.innerText = books.pages
-        list.appendChild(bookDiv)
-        bookDiv.appendChild(bookTitle)
-        bookDiv.appendChild(bookAuthor)
-        bookDiv.appendChild(bookPages)
-        bookDiv.appendChild(removeBtn)
-    }
-}
+  // Function for removing item from HTML
+  const removeFromArray = (event) => {
+    // Removing the book using its button index attribute
+    myLibrary.splice(event.target.attributes[2].nodeValue, 1);
 
-addBookButton.addEventListener('click', createForm)
-// submitButton.addEventListener('click', storeInArray)
+    // Reupdating HTML to display updated contents
+    updatingHTML();
+  };
+
+  // Looping through array and giving each removeBtn correlation the right object 
+  for (let i = 0; i < myLibrary.length; i++) {
+    let bookDiv = document.createElement("div");
+    let bookTitle = document.createElement("h5");
+    let bookAuthor = document.createElement("p");
+    let bookPages = document.createElement("p");
+    let removeBtn = document.createElement("button");
+    removeBtn.setAttribute("type", "button");
+    removeBtn.setAttribute("name", "removeBtn");
+    removeBtn.insertAdjacentText("beforeend", "Remove");
+    removeBtn.setAttribute("index", i);
+    removeBtn.addEventListener("click", removeFromArray);
+
+    let books = myLibrary[i];
+    bookTitle.innerText = books.title;
+    bookAuthor.innerText = books.author;
+    bookPages.innerText = books.pages;
+    list.appendChild(bookDiv);
+    bookDiv.appendChild(bookTitle);
+    bookDiv.appendChild(bookAuthor);
+    bookDiv.appendChild(bookPages);
+    bookDiv.appendChild(removeBtn);
+  }
+};
+
+addBookButton.addEventListener("click", createForm);
